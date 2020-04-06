8a41a3a05abf170c8c9b5c3d2a01d00da63b5777
Apirak Panatkool <apirakb@gmail.com>
2020-04-06 07:35:22 +0700

    add scale line

--- src/plugin/lineUtility.ts
+++ src/plugin/lineUtility.ts
@@ -4,73 +4,94 @@ import { addText } from './textUtility';
 
 export async function addLineNearSelected(type:string):Promise<void> {
   const margin = 8;
-  const textWidth = 32;
-  const textHeight = 14;
   let elementNode = <Rectangle> selectedFirstNode();
-  let scaleX = 1;
-  let scaleY = 1;
-  let scaleWidth = margin + textWidth;
-  let scaleHeight = 1;  
 
   const frame:FrameNode = figma.createFrame();
-
-  switch(type) {
-    case "width":
-      scaleX = elementNode.node.x + elementNode.node.width;
-      scaleY = elementNode.node.y + elementNode.node.height + margin;
-      scaleHeight = elementNode.node.width;
-      frame.rotation = -90;
-      break;
-    case "height":
-      scaleX = elementNode.node.x + elementNode.node.width + margin;
-      scaleY = elementNode.node.y;
-      scaleHeight = elementNode.node.height;
-      break;
-  }  
-
-  frame.resizeWithoutConstraints(scaleWidth, scaleHeight);
-  frame.x = scaleX;
-  frame.y = scaleY;
-
   const fills:Paint[] = [];
   frame.fills = fills;
 
   const centerLine:LineNode = figma.createLine()
-  frame.appendChild(centerLine)
-  centerLine.name = "center line";
-  centerLine.x = margin/2;
-  centerLine.y = 1 ;
-  centerLine.resizeWithoutConstraints(scaleHeight, 0);
-  centerLine.rotation = -90;
-  centerLine.constraints = <Constraints> {horizontal: "MIN", vertical:"STRETCH"};
-  
-  const headLine:LineNode = figma.createLine()
-  frame.appendChild(headLine);
-  headLine.name = "head line";
-  headLine.x = 1;
-  headLine.y = 1;
-  headLine.resizeWithoutConstraints(margin, 0);
-  headLine.constraints = <Constraints> {horizontal: "MIN", vertical:"MIN"};
+  centerLine.name = "center line";  
 
-  const tailLine:LineNode = figma.createLine()
-  frame.appendChild(tailLine);
-  tailLine.name = "tail line";
-  tailLine.x = 1;
-  tailLine.y = scaleHeight;
-  tailLine.resizeWithoutConstraints(margin, 0);
-  tailLine.constraints = <Constraints> {horizontal: "MIN", vertical:"MAX"};
+  const headLine:LineNode = figma.createLine()  
+  headLine.name = "head line";  
 
-  const textNode = await addText("bank", margin, Math.round((scaleHeight-textHeight)/2));
-  frame.appendChild(textNode)
+  const tailLine:LineNode = figma.createLine()  
+  tailLine.name = "tail line";  
+
+  let lenght = 0
+  if (type == "height") { lenght = elementNode.node.height}
+  if (type == "width") { lenght = elementNode.node.width}
+  const textNode = await addText(String(lenght), margin, 1);
+  tailLine.name = "lenght";
+  textNode.textAlignHorizontal = "CENTER";
+  textNode.textAlignVertical = "CENTER";   
 
   switch(type) {
-    case "width":
-      textNode.rotation = 90;
-      break;
     case "height":
-      textNode.rotation = 0;
+      frame.x = elementNode.node.x + elementNode.node.width + margin;
+      frame.y = elementNode.node.y;
+      frame.name = "height"
+      frame.resizeWithoutConstraints(100, elementNode.node.height); 
+
+      centerLine.x = Math.round((margin/2)-1);
+      centerLine.y = 1 ;
+      centerLine.resizeWithoutConstraints(elementNode.node.height, 0);
+      centerLine.rotation = -90;
+      centerLine.constraints = <Constraints> {horizontal: "MIN", vertical:"STRETCH"};  
+      
+      headLine.x = 0;
+      headLine.y = 1;
+      headLine.resizeWithoutConstraints(margin-1, 0);
+      headLine.constraints = <Constraints> {horizontal: "MIN", vertical:"MIN"};
+      
+      tailLine.x = 0;
+      tailLine.y = elementNode.node.height;
+      tailLine.resizeWithoutConstraints(margin-1, 0);
+      tailLine.constraints = <Constraints> {horizontal: "MIN", vertical:"MAX"};
+
+      textNode.y = Math.round((elementNode.node.height-textNode.height)/2); 
+      textNode.x = margin
+      textNode.constraints = <Constraints> {horizontal: "MIN", vertical:"CENTER"};
+
+      frame.resizeWithoutConstraints(margin+textNode.width, elementNode.node.height); 
       break;
-  }
+    case "width":
+      frame.x = elementNode.node.x;
+      frame.y = elementNode.node.y + elementNode.node.height + margin;
+      frame.name = "width"
+      frame.resizeWithoutConstraints(100, elementNode.node.width); 
+
+      centerLine.x = 1;
+      centerLine.y = Math.round(margin/2);
+      centerLine.resizeWithoutConstraints(elementNode.node.width, 0);
+      centerLine.constraints = <Constraints> {horizontal: "STRETCH", vertical:"MIN"};
+
+      headLine.x = 0;
+      headLine.y = 0;
+      headLine.resizeWithoutConstraints(margin-1, 0);
+      headLine.rotation = -90;
+      headLine.constraints = <Constraints> {horizontal: "MIN", vertical:"MIN"};
+
+      tailLine.x = elementNode.node.width-1;
+      tailLine.y = 0;
+      tailLine.resizeWithoutConstraints(margin-1, 0);
+      tailLine.rotation = -90;
+      tailLine.constraints = <Constraints> {horizontal: "MAX", vertical:"MIN"};   
+        
+      // textNode.rotation = 0;
+      textNode.x = Math.round((elementNode.node.width-textNode.width)/2);
+      textNode.y = margin;
+      textNode.constraints = <Constraints> {horizontal: "CENTER", vertical:"MIN"};
+
+      frame.resizeWithoutConstraints(elementNode.node.width, margin+textNode.height);
+      break;
+  }  
+  
+  frame.appendChild(centerLine)
+  frame.appendChild(headLine);
+  frame.appendChild(tailLine);
+  frame.appendChild(textNode)
 
   if(elementNode.node.parent){
     elementNode.node.parent.appendChild(frame);
